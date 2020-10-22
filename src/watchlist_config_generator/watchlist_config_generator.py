9d9c906b2bd9393014063377f74e4771d5041a58
Jacopo Abbate <jacopo.abbate@peregrinetraders.com>
2020-10-22 19:05:00 +0200

    Added create_instrument_specific_regex function to the file.

--- src/watchlist_config_generator/watchlist_config_generator.py
+++ src/watchlist_config_generator/watchlist_config_generator.py
@@ -6,3 +6,8 @@ def get_source_from_file_name(file_name: str) -> str:
     name_components = file_name.split('_')
     source = name_components[1]
     return source
+
+
+def create_instrument_specific_regex(instrument_name: str) -> str:
+    regex = rf"{instrument_name}\\[A-Z][0-9][0-9]"
+    return regex
