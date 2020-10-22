9a53d13b12ff71006c3fbb5cfe4da420b9d78473
Jacopo Abbate <jacopo.abbate@peregrinetraders.com>
2020-10-22 18:42:32 +0200

    Added test for the get_source_from_file_name function.

--- tests/test_watchlist_config_generator.py
+++ tests/test_watchlist_config_generator.py
@@ -1,3 +1,19 @@
 import pytest
 
-class TestExctactAvailableInstances
+from watchlist_config_generator import watchlist_config_generator as wcg
+
+
+class TestGetSourceFromFileName:
+    @pytest.mark.parametrize(
+        'file_name, source', [
+            ('COREREF_207_20201016.txt.bz2', '207'),
+            ('COREREF_945_20200815.txt.bz2', '945')
+        ]
+    )
+    def test_retrieval_of_source_code(self, file_name, source):
+        # Setup
+        # Exercise
+        retrieved_source_code = wcg.get_source_from_file_name(file_name)
+        # Verify
+        assert retrieved_source_code == source
+        # Cleanup - none
