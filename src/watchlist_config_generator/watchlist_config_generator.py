b2b138161913d4f0fb56cd3c628fa1fbba697c9e
Jacopo Abbate <jacopo.abbate@peregrinetraders.com>
2020-10-22 19:16:01 +0200

    Added create_list_of_instrument_regexes function and modified the existing one-line functions.
    The commit introduces the new function named create_list_of_instrument_regexes and modifies the return statements of the two existing one-line functions to avoid unnecessary creation of temporary objects that are then returned by the function.

--- src/watchlist_config_generator/watchlist_config_generator.py
+++ src/watchlist_config_generator/watchlist_config_generator.py
@@ -4,10 +4,12 @@ from typing import List
 
 def get_source_from_file_name(file_name: str) -> str:
     name_components = file_name.split('_')
-    source = name_components[1]
-    return source
+    return name_components[1]
 
 
 def create_instrument_specific_regex(instrument_name: str) -> str:
-    regex = rf"{instrument_name}\\[A-Z][0-9][0-9]"
-    return regex
+    return rf"{instrument_name}\\[A-Z][0-9][0-9]"
+
+
+def create_list_of_instrument_regexes(instrument_names: List) -> List:
+    return [create_instrument_specific_regex(name) for name in instrument_names]
