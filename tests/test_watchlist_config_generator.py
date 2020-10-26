c38c163512055b16fabe66cd7f933c0c74ad3f9e
Jacopo Abbate <jacopo.abbate@peregrinetraders.com>
2020-10-26 16:38:55 +0100

    Added test for the retrieve_source_name_pairs function.

--- tests/test_watchlist_config_generator.py
+++ tests/test_watchlist_config_generator.py
@@ -4,22 +4,6 @@ import pytest
 from watchlist_config_generator import watchlist_config_generator as wcg
 
 
-class TestExtrapolateSourceInstrumentsView:
-    def test_extrapolation_of_source_instrument_view(self, get_source_symbols_dict):
-        # Setup
-        path_to_instruments_file = (
-            pathlib.Path(__file__).resolve().parent / 'static_data' / 'instruments.json'
-        )
-        # Exercise
-        extrapolated_source_instrument_view = wcg.extrapolate_source_instruments_view(
-            path_to_instruments_file
-        )
-        # Verify
-        expected_source_instrument_view = get_source_symbols_dict
-        assert extrapolated_source_instrument_view == expected_source_instrument_view
-        # Cleanup - none
-
-
 class TestDiscoverReferenceDataFiles:
     def test_discovery_of_reference_data_files(self, tmp_path):
         # Setup
@@ -53,6 +37,22 @@ class TestDiscoverReferenceDataFiles:
         # Cleanup - none
 
 
+class TestExtrapolateSourceInstrumentsView:
+    def test_extrapolation_of_source_instrument_view(self, get_source_symbols_dict):
+        # Setup
+        path_to_instruments_file = (
+            pathlib.Path(__file__).resolve().parent / 'static_data' / 'instruments.json'
+        )
+        # Exercise
+        extrapolated_source_instrument_view = wcg.extrapolate_source_instruments_view(
+            path_to_instruments_file
+        )
+        # Verify
+        expected_source_instrument_view = get_source_symbols_dict
+        assert extrapolated_source_instrument_view == expected_source_instrument_view
+        # Cleanup - none
+
+
 class TestGetSourceFromFilePath:
     @pytest.mark.parametrize(
         'file_path, source', [
@@ -74,11 +74,12 @@ class TestRetrieveInstruments:
         'source, expected_list_of_instruments', [
             (
                 "207",
-                ["F:FDAX", "F:FSMI", "F:FESX", "F:FGBS", "F:FGBM", "F:FGBL", "F:FGBX", "F:FBTS",
-                 "F:FBTP", "F:FOAT"]
+                ["F:FBTP", "F:FBTS", "F:FDAX", "F:FESX", "F:FGBL",
+                 "F:FGBM", "F:FGBS", "F:FGBX", "F:FOAT", "F:FSMI"
+                 ]
              ),
             (
-                "367", ["F2:ZF", "F2:ZN", "F2:ZT", "F2:UB", "F2:ZB", "F2:TN"]
+                "367", ["F2:TN", "F2:UB", "F2:ZB", "F2:ZF", "F2:ZN", "F2:ZT"]
             ),
             (
                 "611", ["F:FCE"]
@@ -95,66 +96,52 @@ class TestRetrieveInstruments:
         # Setup
         source_instruments_view = get_source_symbols_dict
         # Exercise
-        retrieved_instruments = wcg.retrieve_instruments(source_instruments_view, source)
+        retrieved_instruments = wcg.retrieve_instruments(source, source_instruments_view)
         # Verify
         assert retrieved_instruments == expected_list_of_instruments
         # Cleanup - none
 
 
-class TestCreateMessageRegex:
-    def test_creation_of_message_regex(self):
-        # Setup
-        source_code = '207'
-        instrument_symbol = 'F:FESX'
-        # Exercise
-        generated_regex = wcg.create_message_regex(source_code, instrument_symbol)
-        # Verify
-        expected_regex = r"^DC\|207\|F:FESX\\\\[A-Z][0-9][0-9]"
-        assert generated_regex == expected_regex
-        # Cleanup - none
-
-
-class TestCreateMessageLevelRegexes:
-    def test_creation_of_message_level_regexes(self):
+class TestCreateMessageLevelPattern:
+    def test_creation_of_message_level_pattern(self):
         # Setup
         instrument_symbols = ["F2:ES", "F2:NQ"]
         source_code = '684'
         # Exercise
-        generated_message_level_regexes = wcg.create_message_level_regexes(
+        generated_message_level_regexes = wcg.create_message_level_pattern(
             source_code,
             instrument_symbols,
         )
         # Verify
-        expected_message_level_regexes = [
-            r"^DC\|684\|F2:ES\\\\[A-Z][0-9][0-9]",
-            r"^DC\|684\|F2:NQ\\\\[A-Z][0-9][0-9]"
-        ]
+        expected_message_level_regexes = (
+            r"^DC\|684\|(F2:ES\\[A-Z][0-9]{2}|F2:NQ\\[A-Z][0-9]{2})"
+        )
         assert generated_message_level_regexes == expected_message_level_regexes
         # Cleanup - none
 
 
-class TestCreateInstrumentSpecificRegex:
+class TestCreateSpecificInstrumentRegex:
     def test_creation_of_instrument_specific_regex(self):
         # Setup
         instrument_name = "F:FBTP"
         # Exercise
-        generated_regexes = wcg.create_instrument_specific_regex(instrument_name)
+        generated_regexes = wcg.create_specific_instrument_regex(instrument_name)
         # Verify
-        correct_regex = "F:FBTP\\\\[A-Z][0-9][0-9]"
+        correct_regex = r"F:FBTP\\[A-Z][0-9]{2}"
         assert generated_regexes == correct_regex
         # Cleanup - none
 
 
-class TestCreateInstrumentLevelRegexes:
-    def test_creation_of_list_of_instrument_regexes(self):
+class TestCreateInstrumentLevelPattern:
+    def test_creation_of__instrument_level_regex(self):
         # Setup
         instrument_names = ['F:FBTP', 'F:FDAX', 'F:FESX']
         # Exercise
-        generated_instrument_regexes = wcg.create_instrument_level_regexes(instrument_names)
+        generated_instrument_regexes = wcg.create_instrument_level_pattern(instrument_names)
         # Verify
-        expected_instrument_regexes = [
-            'F:FBTP\\\\[A-Z][0-9][0-9]', 'F:FDAX\\\\[A-Z][0-9][0-9]', 'F:FESX\\\\[A-Z][0-9][0-9]'
-        ]
+        expected_instrument_regexes = (
+            r'(F:FBTP\\[A-Z][0-9]{2}|F:FDAX\\[A-Z][0-9]{2}|F:FESX\\[A-Z][0-9]{2})'
+        )
         assert generated_instrument_regexes == expected_instrument_regexes
         # Cleanup - none
 
@@ -162,10 +149,40 @@ class TestCreateInstrumentLevelRegexes:
 class TestCombineMultipleRegexes:
     def test_combination_of_regexes(self):
         # Setup
-        regexes = ['F2:ES\\\\[A-Z][0-9][0-9]', 'F2:NQ\\\\[A-Z][0-9][0-9]']
+        regexes = [r'F2:ES\\[A-Z][0-9]{2}', r'F2:NQ\\[A-Z][0-9]{2}']
         # Exercise
         generated_combination_of_regexes = wcg.combine_multiple_regexes(regexes)
         # Verify
-        expected_combination_of_regexes = "F2:ES\\\\[A-Z][0-9][0-9]|F2:NQ\\\\[A-Z][0-9][0-9]"
+        expected_combination_of_regexes = "F2:ES\\\\[A-Z][0-9]{2}|F2:NQ\\\\[A-Z][0-9]{2}"
         assert generated_combination_of_regexes.pattern == expected_combination_of_regexes
+        # Cleanup - none
+
+
+class TestRetrieveSourceNamePairs:
+    def test_retrieval_of_source_name_pairs(self):
+        # Setup
+        path_to_reference_data_file = (
+            pathlib.Path(__file__).resolve().parent / "static_data" / "COREREF_207_20201026.txt.bz2"
+        )
+        instrument_level_regex = (
+            r"(F:FDAX\\[A-Z][0-9]{2}|F:FESX\\[A-Z][0-9]{2}|F:FBTP\\[A-Z][0-9]{2}|"
+            r"F:FBTS\\[A-Z][0-9]{2})"
+        )
+        message_level_regex = (
+            r"^DC\|207\|(F:FDAX\\[A-Z][0-9]{2}|F:FESX\\[A-Z][0-9]{2}|F:FBTP\\[A-Z][0-9]{2}|"
+            r"F:FBTS\\[A-Z][0-9]{2})"
+        )
+        # Exercise
+        retrieved_source_name_pairs = wcg.retrieve_source_name_pairs(
+            path_to_reference_data_file, message_level_regex, instrument_level_regex
+        )
+        # Verify
+        expected_source_name_pairs = [
+            '207,F:FBTP\\H21\n', '207,F:FBTP\\M21\n', '207,F:FBTP\\Z20\n', '207,F:FBTS\\H21\n',
+            '207,F:FBTS\\M21\n', '207,F:FBTS\\Z20\n', '207,F:FDAX\\H21\n', '207,F:FDAX\\M21\n',
+            '207,F:FDAX\\Z20\n', '207,F:FESX\\H21\n', '207,F:FESX\\H22\n', '207,F:FESX\\M21\n',
+            '207,F:FESX\\M22\n', '207,F:FESX\\U21\n', '207,F:FESX\\U22\n', '207,F:FESX\\Z20\n',
+            '207,F:FESX\\Z21\n'
+        ]
+        assert retrieved_source_name_pairs == expected_source_name_pairs
         # Cleanup - none
