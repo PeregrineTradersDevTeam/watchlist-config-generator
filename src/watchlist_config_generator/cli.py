ca333036820628ed8adb2cc41804fbb6155a9fed
Jacopo Abbate <jacopo.abbate@peregrinetraders.com>
2020-10-28 15:32:24 +0100

    Added the makeconfig function to the file.

--- src/watchlist_config_generator/cli.py
+++ src/watchlist_config_generator/cli.py
@@ -1,4 +1,31 @@
 """
 Module containing the command line app.
 """
-import click 
+import pathlib
+
+import click
+
+from watchlist_config_generator.watchlist_config_generator import (
+    config_file_writer,
+    find_all_coreref_files,
+    json_loader,
+    process_all_coreref_files,
+)
+
+
+@click.command()
+@click.argument('data_directory', type=click.Path(exists=True))
+@click.argument('path_to_input_file', type=click.Path(exists=True))
+@click.option("-w", "--write-to", type=click.Path(exists=True), default=None)
+def makeconfig(data_directory, path_to_input_file, write_to):
+    reference_data_files = find_all_coreref_files(data_directory)
+    instruments_input_file = json_loader(path_to_input_file)
+    discovered_contracts = process_all_coreref_files(reference_data_files, instruments_input_file)
+    if not write_to:
+        write_to = pathlib.Path.cwd()
+    summary = config_file_writer(write_to, discovered_contracts)
+    click.echo(summary)
+
+
+if __name__ == '__main__':
+    makeconfig()
